38e9a14e265f378607a9c7e8761b9e3bde63abd6
shravan40 <shravan.ma.iitkgp@gmail.com>
2016-10-01 00:33:41 +0530

    Calculate your age in number of year and months

---
+++ calculate_age.cpp
@@ -0,0 +1,27 @@
+#include "boost/date_time/gregorian/gregorian.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace boost::gregorian;
+
+int main(){
+  string dob;
+  cout << "Please enter your DOB in YY-MM-DD format : ";
+  cin >> dob;
+  date d(from_string(dob));
+  date current = day_clock::local_day();
+
+  int age_in_year = current.year() - d.year();
+  int age_in_month = current.month() - d.month();
+  int age_in_day = current.day() - d.day();
+
+  if(age_in_month < 0) {
+    age_in_year--;
+    age_in_month = 12 + age_in_month;
+  }
+
+  cout << "You are " << age_in_year << "year and " << age_in_month << "month ";
+  cout << "Old now \n";
+  return 0;
+
+}
